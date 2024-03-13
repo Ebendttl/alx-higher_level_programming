a93cbad7fd985d416345f07d04017c093d5b4ece
root <root@Ebendttl>
2024-03-13 14:51:10 +0100

    list tables

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,3 @@
--- A script that lists all the tables of a database in your MySQL server.
+-- A script that lists all the tables of a database in your MySQL server
 -- List tables
 SHOW TABLES FROM mysql;
