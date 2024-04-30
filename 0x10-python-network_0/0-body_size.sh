23665ee3bd7243e20d82190c1435539230b0f7d2
Ebendttl <ebendttl@gmail.com>
2024-04-30 06:16:03 +0100

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script sends a GET request to a URL and displays the response body if the status code is 200
-curl -sL "$1" -w "%{http_code}" -o /dev/null | awk -v url="$1" '$NF == "200" {system("curl -sL " url)}'
+# This script sends a request to a URL and displays the size of the response body in bytes
+curl -sL "$1" -w "%{size_download}" -o /dev/null
