202814162e13d97c72f4f99050a29d972dbc48b5
root <root@Ebendttl>
2024-05-04 14:16:41 +0100

    removed the end='' option

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 # Prints exactly "Programming is like building a multilingual puzzle".
-print('"Programming is like building a multilingual puzzle', end='')
+print('"Programming is like building a multilingual puzzle')
