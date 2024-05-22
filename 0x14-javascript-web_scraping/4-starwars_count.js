f03dc80851ba3f71557fc285a791ff56d21b1f9f
root <root@Ebendttl>
2024-05-22 02:53:27 +0100

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,2 +1,27 @@
 #!/usr/bin/node
 
+const request = require('request');
+
+// Get the API URL from the command-line arguments
+const apiUrl = process.argv[2];
+
+// Wedge Antilles character ID
+const wedgeAntillesId = '18';
+
+// Make a GET request to the API URL
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    // Parse the response body as JSON
+    const movies = JSON.parse(body).results;
+
+    // Filter the movies that have Wedge Antilles as a character
+    const moviesWithWedgeAntilles = movies.filter((movie) => {
+      return movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesId}/`);
+    });
+
+    // Print the count of movies with Wedge Antilles
+    console.log(moviesWithWedgeAntilles.length);
+  }
+});
