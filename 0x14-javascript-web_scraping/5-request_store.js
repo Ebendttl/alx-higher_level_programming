53814b6a863b54ebe4736fe90b61431e2f7c96a8
root <root@Ebendttl>
2024-05-22 03:08:11 +0100

    Task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,2 +1,24 @@
 #!/usr/bin/node
 
+const fs = require('fs');
+const request = require('request');
+
+// Get the URL and file path from the command-line arguments
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+// Make a GET request to the URL
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    // Write the response body to the file
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`File saved: ${filePath}`);
+      }
+    });
+  }
+});
