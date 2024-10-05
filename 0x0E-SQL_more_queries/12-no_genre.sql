5ab7ce52d39df353b8650f730aeee7bb7d16ca49
root <root@Ebendttl>
2024-10-05 03:55:43 +0100

    task 12: fix 1

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,9 +1,11 @@
--- A script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
--- No genre
-SELECT h.title, s.genre_id
-FROM hbtn_0d_tvshows h
-LEFT JOIN hbtn_0d_tvshows s ON h.title = s.title
-WHERE s.genre_id IS NULL
-GROUP BY h.title, s.genre_id
-ORDER BY h.title ASC;
-
+-- lists all shows contained in hbtn_0d_tvshows without a genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+SELECT tv_shows.title, tv_show_genres.genre_id
+  FROM tv_shows
+  LEFT JOIN tv_show_genres
+  ON tv_shows.id = tv_show_genres.show_id
+  WHERE tv_show_genres.show_id IS NULL
+  ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
