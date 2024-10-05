b6077c11da8d2106ce96b793a29be90106143063
root <root@Ebendttl>
2024-10-05 02:36:27 +0100

    task

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,4 @@
--- A script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
--- Full description
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
-
+-- prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server
+-- The database name will be passed as an argument of the mysql command
+-- You are not allowed to use the DESCRIBE or EXPLAIN statements
+SHOW CREATE TABLE first_table;
