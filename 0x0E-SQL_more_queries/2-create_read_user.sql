5ece5a38cb1e219944dce8d6d386747560ec3373
root <root@Ebendttl>
2024-10-05 03:33:28 +0100

    task

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,14 +1,7 @@
--- A script that creates the database hbtn_0d_2 and the user user_0d_2
--- Read user
--- Create the database hbtn_0d_2 if it doesn't exist
+-- creates the database hbtn_0d_2 and the user user_0d_2.
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+-- The user_0d_2 password should be set to user_0d_2_pwd
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
--- Create the user user_0d_2 if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
-
--- Grant SELECT privilege on the database hbtn_0d_2 to the user user_0d_2
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
-
--- Flush privileges
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
-
