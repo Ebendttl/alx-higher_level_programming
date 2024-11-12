b3746276341b4dcea964bf146ab3ff402c2a0823
root <root@Ebendttl>
2024-11-12 19:32:00 +0100

    Reupdated the file

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,24 +1,39 @@
 #!/usr/bin/python3
 """
-Lists all City objects from the database hbtn_0e_101_usa
+Module Docs
 """
-import sys
+from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session, sessionmaker
 
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+def main():
+    """
+    Function DOC
+    """
+    db_user = argv[1]
+    db_password = argv[2]
+    db_db = argv[3]
+    db_host = "localhost"
 
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}'
+            .format(db_user,
+                    db_password,
+                    db_host,
+                    db_db),
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    st = session.query(State).join(City).order_by(City.id).all()
-
-    for state in st:
+    states = session.query(State).join(City).order_by(City.id).all()
+    for state in states:
         for city in state.cities:
             print("{}: {} -> {}".format(city.id, city.name, state.name))
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
