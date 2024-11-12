f15fccceabe73d050e269ab3a80b23ff76e050c2
root <root@Ebendttl>
2024-11-12 19:33:34 +0100

    Reupdated the file

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,39 +1,22 @@
 #!/usr/bin/python3
 """
-Module Docs
+Lists all City objects from the database hbtn_0e_101_usa.
+Usage: ./102-relationship_cities_states_list.py <mysql username> /
+                                                <mysql password> /
+                                                <database name>
 """
-from sys import argv
-from relationship_state import Base, State
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
 from relationship_city import City
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import Session, sessionmaker
-
-
-def main():
-    """
-    Function DOC
-    """
-    db_user = argv[1]
-    db_password = argv[2]
-    db_db = argv[3]
-    db_host = "localhost"
-
-    engine = create_engine(
-            'mysql+mysqldb://{}:{}@{}/{}'
-            .format(db_user,
-                    db_password,
-                    db_host,
-                    db_db),
-            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    states = session.query(State).join(City).order_by(City.id).all()
-    for state in states:
-        for city in state.cities:
-            print("{}: {} -> {}".format(city.id, city.name, state.name))
-    session.close()
-
 
 if __name__ == "__main__":
-    main()
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for city in session.query(City).order_by(City.id):
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
